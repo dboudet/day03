210f6c080ef683ac280f8b7e700484a3c3b43a3a
Dan Boudet <dboudet@palmbeachculture.com>
2021-07-01 09:27:53 -0400

    fixed fizzbuzz with else-if per Todd

--- homework/fizzBuzz.js
+++ homework/fizzBuzz.js
@@ -1,3 +1,27 @@
+
+// else-if
+
+const stopLimit = 100
+
+function fizzBuzz(whenToStop){
+    for( let i=1; i<=whenToStop; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("Fizz Buzz")
+        } else if (i % 3 === 0 && i % 5 !== 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)            
+        }
+    }
+}
+
+fizzBuzz(stopLimit)
+ 
+
+// ifs
+/* 
 const stopLimit = 100
 
 function fizzBuzz(whenToStop){
@@ -8,10 +32,14 @@ function fizzBuzz(whenToStop){
             console.log("Fizz")
         } if (i % 5 === 0 && i % 3 !== 0) {
             console.log("Buzz")
-        } if (i % 5 !== 0 && i %3 !== 0) { // using else overlapped with 3s?
+        } if (i % 3 !== 0 && i % 5 !== 0) { 
             console.log(i)            
         }
     }
 }
 
 fizzBuzz(stopLimit)
+ */
+
+// you can use ternary parts of it - the simpler ifs
+// console.log((i%5===0) ? 'Buzz' :i)
